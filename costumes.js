a941a2f90793d970e4c88fe124b879edae41f8a4
root <root@raspberrypi.(none)>
2015-04-16 02:41:07 +0000

    Reducing categories to just first one.

--- costumes.js
+++ costumes.js
@@ -14,10 +14,11 @@ if (process.argv.length != 3) {
 	var costume = costumes[i];
         var name = costume.name;
 	name = name.split(' ').join('_');
+	name = name.split("'").join('_');
 	var ext = costume.md5.split('.')[1];
 	name = name + "." + ext;
 	var file_url = "http://cdn.assets.scratch.mit.edu/internalapi/asset/" + costume.md5 + "/get/";
-	for (var t=0; t < costume.tags.length; t++) {
+	for (var t=0; t < 1; t++) {
 	    var tag = costume.tags[t];
 	    console.log("mkdir " + tag);
 	    console.log("wget -O " + tag + "/" + name + " " + file_url);
